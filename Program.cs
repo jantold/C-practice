ec122c12ad8fc2748f3dae151684c41845cf8cb3
Jan296929B <jan.berchtold@htwg-konstanz.de>
2023-04-09 19:28:17 +0900

    testing C# types

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Person
+{
+    // Constructor that takes no arguments:
+    public Person()
+    {
+        Name = "unknown";
+    }
+
+    // Constructor that takes one argument:
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    // Auto-implemented readonly property:
+    public string Name { get; }
+
+    // Method that overrides the base class (System.Object) implementation.
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+class TestPerson
+{
+    static void Main()
+    {
+        // Call the constructor that has no parameters.
+        var person1 = new Person();
+        Console.WriteLine(person1.Name);
+
+        // Call the constructor that has one parameter.
+        var person2 = new Person("Sarah Jones");
+        Console.WriteLine(person2.Name);
+        // Get the string representation of the person2 instance.
+        Console.WriteLine(person2);
+
+        int threshhold = 3;
+        int[] source = {1, 2, 3, 4, 5 };
+        var query = from item in source
+                    where item <= threshhold 
+                    select item;
+        foreach (var item in query)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+// Output:
+// unknown
+// Sarah Jones
+// Sarah Jones
